532124205f10248bf1eaa41ea1d9b6b48e9e16fc
GitHub Action <action@github.com>
2021-08-03 09:56:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yigepang/armstemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yigepang.armstemplateplugin.services
+
+import com.github.yigepang.armstemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
